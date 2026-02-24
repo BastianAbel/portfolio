2f27a908f4627676d93b7b21e98d47a9a9bd240f
Bastian Abel <dev.bastianabel@gmail.com>
2026-02-24 13:53:52 +0100

    header animation and position sticky

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+function toggleVisibility(id) {
+    const elementRef = document.getElementById(id);
+    elementRef.classList.toggle('d-none');
+}
+
+// function machmal() {
+//     const elementRef = document.getElementById('header-logo');
+//     elementRef.classList.toggle('scrolled');
+// }
